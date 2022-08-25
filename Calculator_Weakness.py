2b42d7777c2d4c71fc421417cea042543e7976cb
Alexander Krauß <alexander.krauss98@gmail.com>
2022-08-25 18:44:00 +0100

    Update Calculator_Weakness.py

--- Calculator_Weakness.py
+++ Calculator_Weakness.py
@@ -1,81 +1,19 @@
-#--------------------------------------------------------------------------------------------------------Pokemon library----------------------------
-all_Types_Eng = {"Normal","Fighting","Flying","Poison","Ground","Rock","Bug",
-                 "Ghost","Steel","Fire","Water","Grass","Electric",
-                 "Psychic","Ice","Dragon","Dark","Fairy"} #check
+#-----------------------------------------------------------------------Import from Library -----------------------------
+from PokemonLib import all_Types_Eng
+from PokemonLib import Weakdic
+from PokemonLib import Resdic
+from PokemonLib import Nothdic
 
-Weakdic = {
-        "Normal" : [],
-        "Psychic":["Fighting", "Poison"],
-        "Bug":["Psychic","Dark","Grass"],
-        "Fighting":["Normal","Rock","Steel","Ice","Dark"],
-        "Ice":["Flying","Ground","Grass","Dragon"],
-        "Fairy":["Fighting","Dragon","Dark"],
-        "Poison":["Grass","Fairy"],
-        "Ghost":["Ghost","Psychic"],
-        "Grass":["Ground","Rock","Water"],
-        "Dark":["Ghost","Psychic"],
-        "Rock":["Flying","Bug","Fire","Ice"],
-        "Fire":["Bug","Steel","Grass","Ice"],
-        "Flying":["Fighting","Bug","Grass"],
-        "Dragon":["Dragon"],
-        "Ground":["Poison", "Rock","Steel","Fire","Electric"],
-        "Electric":["Flying","Water"],
-        "Steel":["Rock","Ice","Fairy"],
-        "Water":["Ground","Rock","Fire"],
-        }
-        
-Resdic = {
-        "Normal" : ["Rock","Steel"],
-        "Psychic":["Psychic","Steel"],
-        "Bug":["Fire","Fighting","Poison","Flying","Ghost","Steel"],
-        "Fighting":["Poison","Flying","Psychic","Bug","Fairy","Ghost"],
-        "Ice":["Fire","Water","Ice","Steel"],
-        "Fairy":["Fire","Fighting","Steel"],
-        "Poison":["Poison","Ground","Rock","Ghost","Steel"],
-        "Ghost":["Normal","Steel"],
-        "Grass":["Fire","Grass","Poison","Flying","Bug","Dragon"],
-        "Dark":["Fighting","Dark","Fairy"],
-        "Rock":["Fighting","Ground","Steel"],
-        "Fire":["Fire","Water","Rock","Dragon"],
-        "Flying":["Electric","Rock","Steel"],
-        "Dragon":["Steel","Fairy"],
-        "Ground":["Grass","Flying","Bug"],
-        "Electric":["Grass","Electric","Ground","Dragon"],
-        "Steel":["Fire","Water","Electric","Steel"],
-        "Water":["Water","Grass","Dragon"],
-        }
-Nothdic = {
-    "Normal":["Ghost"],
-    "Psychic":["Dark"],
-    "Bug":[],
-    "Fighting":["Ghost"],
-    "Ice":[],
-    "Fairy":[],
-    "Poison":["Steel"],
-    "Ghost":["Normal"],
-    "Grass":[],
-    "Dark":[],
-    "Rock":[],
-    "Fire":[],
-    "Flying":[],
-    "Dragon":["Fairy"],
-    "Ground":["Flying"],
-    "Electric":["Ground"],
-    "Steel":[],
-    "Water":[],
-    }
-    
-#---------------------------------------------------------------------------------------------------------------Type Calculator--------------------
+#-----------------------------------------------------------------------Calculator------------------------------------
 
-#for Carmesin/Purpur: Type_Comp_Eng = ([[a,b,c] for a in all_Types_Eng for b in all_Types_Eng if b != a for c in all_Types_Eng if c != a and c != b]) 
-Type_Comp_Eng = ([[a,b] for a in all_Types_Eng for b in all_Types_Eng if b != a ]) 
-    
+Type_Comp_Eng = ([[a,b] for a in all_Types_Eng for b in all_Types_Eng if b != a ]) #create List of TypeComps
+#for Carmesin/Purpur: Type_Comp_Eng = ([[a,b,c] for a in all_Types_Eng for b in all_Types_Eng if b != a for c in all_Types_Eng if c != a and c != b])    
              
 for comps in Type_Comp_Eng:
-    if [comps[1],comps[0]] in Type_Comp_Eng:# remove dublicates #check 
+    if [comps[1],comps[0]] in Type_Comp_Eng:# remove dublicates
         Type_Comp_Eng.remove(comps)  
 
-for t in Type_Comp_Eng: #create Weakness, Resistent and Negates
+for t in Type_Comp_Eng: #create Weakness, Resistenz and Negates
     weak = []
     res = []
     noth = []
@@ -87,6 +25,7 @@ for t in Type_Comp_Eng: #create Weakness, Resistent and Negates
                 res.append(Atk)
             if def_type in Nothdic[Atk]:
                 noth.append(Atk)
+                
     for i in weak: #clear negates
         if i in noth:
             weak.remove(i)
@@ -100,3 +39,4 @@ for t in Type_Comp_Eng: #create Weakness, Resistent and Negates
     print ("Resistent: " + str(res))
     print ("Doesn't Effect: " + str(noth))
     print ("")
+    
